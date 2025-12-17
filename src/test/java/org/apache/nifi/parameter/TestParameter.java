8b20ea8105f46ec5a86e247218170132ee0714a5
Bryan Bende <bbende@gmail.com>
2025-12-17 16:28:41 -0500

    NIFI-15315 Add support for assets in Connectors (#31)
    - Add ownerIdentifier in Asset and deprecate parameterContextIdentifier
    - Add ASSET and ASSET_LIST types for Connector properties and update validation
    - Support multiple asset references
    
    Signed-off-by: Kevin Doran <kdoran@apache.org>

--- src/test/java/org/apache/nifi/parameter/TestParameter.java
+++ src/test/java/org/apache/nifi/parameter/TestParameter.java
@@ -201,6 +201,11 @@ public class TestParameter {
         }
 
         @Override
+        public String getOwnerIdentifier() {
+            return parameterContextIdentifier;
+        }
+
+        @Override
         public String getName() {
             return name;
         }
