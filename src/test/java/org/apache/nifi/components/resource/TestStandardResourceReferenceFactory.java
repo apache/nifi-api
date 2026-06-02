6913182da3ef285aa1f926cb6ec460d9dbf65abc
dan-s1 <dstieg1@gmail.com>
2026-06-02 11:12:06 -0400

    NIFI-15982 Fixed StandardResourceReferenceFactory disambiguation for File path and comment prefix (#90)
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- src/test/java/org/apache/nifi/components/resource/TestStandardResourceReferenceFactory.java
+++ src/test/java/org/apache/nifi/components/resource/TestStandardResourceReferenceFactory.java
@@ -22,8 +22,10 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -67,6 +69,7 @@ public class TestStandardResourceReferenceFactory {
 
         assertEmptyResourceReferences(resourceReferences);
     }
+
     @Test
     public void testCreateResourceReferencesWhenResourceDefinitionIsNull() {
         String value = "/dir1/test1.jar";
@@ -75,6 +78,23 @@ public class TestStandardResourceReferenceFactory {
         ResourceReferences resourceReferences = subject.createResourceReferences(value, resourceDefinition);
 
         assertEmptyResourceReferences(resourceReferences);
+    }
+
+    @Test
+    public void testDisambiguationBetweenTextAndFile() {
+        final String transformWithSingleLineComment = """
+                // This is a single line comment in JSLT
+                {
+                "id": .userId,
+                "name": .firstName
+                }
+                """;
+
+        final ResourceDefinition resourceDefinition =
+                new StandardResourceDefinition(ResourceCardinality.SINGLE, Set.of(ResourceType.FILE, ResourceType.TEXT));
+        final ResourceReference resourceReference = subject.createResourceReference(transformWithSingleLineComment, resourceDefinition);
+
+        assertInstanceOf(Utf8TextResource.class, resourceReference);
     }
 
     private StandardResourceDefinition createResourceDefinition() {
